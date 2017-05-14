f4c3bc511b21a2946963a84dfa8a017a2e6b8c6b
Alexandre Ouellette <alexandre.f.ouellette@gmail.com>
2017-05-13 21:41:03 -0400

    Added notes to the seed

--- server/util/seed.js
+++ server/util/seed.js
@@ -1,6 +1,7 @@
 var User = require('../api/user/userModel');
 var Project = require('../api/project/projectModel');
 var Category = require('../api/category/categoryModel');
+var Note = require('../api/note/noteModel');
 var _ = require('lodash');
 var logger = require('./logger');
 
@@ -16,6 +17,12 @@ var categories = [
   {name: 'intros'},
   {name: 'angular'},
   {name: 'UI/UX'}
+];
+
+var notes = [
+  {body: 'This is a sample note'},
+  {body: 'This is a note with quite the long body, it will contain quite a bit of text. How are you? I am pretty good actually. That is great!!'},
+  {body: 'Note with a title', title: 'Note title ... very original'}
 ];
 
 var projects = [
@@ -34,7 +41,7 @@ var createDoc = function(model, doc) {
 
 var cleanDB = function() {
   logger.log('... cleaning the DB');
-  var cleanPromises = [User, Category, Project]
+  var cleanPromises = [User, Category, Project, Note]
     .map(function(model) {
       return model.remove().exec();
     });
@@ -64,6 +71,18 @@ var createCategories = function(data) {
     });
 };
 
+var createNotes = function(data) {
+  var promises = notes.map(function(note) {
+    return createDoc(Note, note);
+  });
+
+  return Promise.all(promises)
+    .then(function(notes) {
+      return _.merge({notes: notes}, data || {});
+    });
+};
+
+
 var createProjects = function(data) {
 
   var newProjects = projects.map(function(project, i) {
@@ -73,13 +92,14 @@ var createProjects = function(data) {
 
   return Promise.all(newProjects)
     .then(function() {
-      return 'Seeded DB with 3 Projects, 3 Users, 3 Categories';
+      return 'Seeded DB with 3 Projects, 3 Users, 3 Categories and 3 Notes';
     });
 };
 
 cleanDB()
   .then(createUsers)
   .then(createCategories)
+  .then(createNotes)
   .then(createProjects)
   .then(logger.log.bind(logger))
   .catch(logger.log.bind(logger));
